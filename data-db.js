fb78fab46d1880d8541383e23d954df5a662d7d2
ycxukun <53800412+ycxukun@users.noreply.github.com>
2026-06-30 09:47:45 +0800

    15.3

--- data-db.js
+++ data-db.js
@@ -20,7 +20,8 @@
 
 
 
-const APP_VERSION = 'V15.1 管理员在线保存版｜清理旧备注界面｜公开只读';
-const CACHE_VERSION = '20260629-v15-1-clean-admin';
+
+const APP_VERSION = 'V15.2 显式管理员备注版｜公开只读｜管理员可新增';
+const CACHE_VERSION = '20260629-v15-2-visible-admin-note';
 const DB = JSON.parse(window.__DB_JSON_PARTS.join(''));
 window.__DB_JSON_PARTS = null;
