e85ba06c9810b79fabe684202682d39de687bf17
ycxukun <53800412+ycxukun@users.noreply.github.com>
2026-06-30 09:42:13 +0800

    15.2

--- data-db.js
+++ data-db.js
@@ -19,7 +19,8 @@
 
 
 
-const APP_VERSION = 'V15 管理员在线保存版｜GitHub同步备注｜公开只读';
-const CACHE_VERSION = '20260629-v15-admin-github-sync';
+
+const APP_VERSION = 'V15.1 管理员在线保存版｜清理旧备注界面｜公开只读';
+const CACHE_VERSION = '20260629-v15-1-clean-admin';
 const DB = JSON.parse(window.__DB_JSON_PARTS.join(''));
 window.__DB_JSON_PARTS = null;
